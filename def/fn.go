93d9c4bd038c28453398b15c23c42ec280747bbf
Petar Maymounkov <petarm@gmail.com>
2022-02-11 15:53:17 -0800

    add call and return types

--- def/fn.go
+++ def/fn.go
@@ -12,3 +12,29 @@ func (fn Fn) Deps() Types {
 func (Fn) Kind() string {
 	return "Fn"
 }
+
+type Call struct {
+	Fn Fn
+	ID Type // function instance identifier can be user-defined
+}
+
+func (call Call) Deps() Types {
+	return Types{call.Fn}
+}
+
+func (Call) Kind() string {
+	return "Call"
+}
+
+type Return struct {
+	Fn Fn
+	ID Type // function instance identifier can be user-defined
+}
+
+func (r Return) Deps() Types {
+	return Types{r.Fn}
+}
+
+func (Return) Kind() string {
+	return "Return"
+}
