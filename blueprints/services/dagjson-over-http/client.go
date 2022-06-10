a02353c71c3c8c3b49bbc872afb5a3e2c2ebb7cd
Petar Maymounkov <petarm@gmail.com>
2022-06-10 11:14:55 -0700

    fix

--- blueprints/services/dagjson-over-http/client.go
+++ blueprints/services/dagjson-over-http/client.go
@@ -296,7 +296,7 @@ func {{.ProcessReturnAsync}}(ctx {{.Context}}, ch chan<- {{.MethodReturnAsync}},
 			out = {{.MethodReturnAsync}}{Err: {{.ErrProto}}{Cause: err}} // IPLD decode error
 		} else {
 			var x [1]byte
-			if k, err := r.Read(x[:]); err != nil || k != 1 || x[0] != '\n' {
+			if k, err := r.Read(x[:]); k != 1 || x[0] != '\n' {
 				out = {{.MethodReturnAsync}}{Err: {{.ErrProto}}{Cause: {{.Errorf}}("missing new line after result: err (%v), read (%d), char (%q)", err, k, string(x[:]))}} // Edelweiss decode error
 			} else {
 				env := &{{.ReturnEnvelope}}{}
